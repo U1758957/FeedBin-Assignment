7aef033a8a1cd0655e75c4038df6b1016ab41af4
UggUggDev <10110halp@gmail.com>
2022-01-21 12:07:54 +0000

    FeedBinGUI class JavaDoc created

--- src/gui/FeedBinGUI.java
+++ src/gui/FeedBinGUI.java
@@ -2,5 +2,15 @@ package gui;
 
 import javax.swing.*;
 
+/**
+ * Anne Grundy
+ *
+ * Modified by Gary Allen
+ * Modified by Zac King (U1758957)
+ *
+ * Latest update January 2022
+ * Version 1.2
+ *
+ */
 public class FeedBinGUI extends JFrame {
 }
