bc415e9a58a773a1bd9f36ce52db2a36feeb3734
UggUggDev <10110halp@gmail.com>
2022-01-21 11:33:32 +0000

    Class JavaDoc added

--- src/bin/FeedBin.java
+++ src/bin/FeedBin.java
@@ -1,4 +1,18 @@
 package bin;
 
+/**
+ *
+ * FeedBin.java
+ * A R Grundy
+ *
+ * Modified by Gary Allen,
+ * Modified by Zac King (U1758957)
+ *
+ * Last update January 2022
+ *
+ * A class for modelling the state and operation of an "animal feed bin"
+ * as used in the factory production of animal feedstuffs
+ *
+ */
 public class FeedBin {
 }
