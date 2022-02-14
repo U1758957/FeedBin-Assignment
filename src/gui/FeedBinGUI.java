7c13932710b84154addeea5b31755968677be4b6
UggUggDev <10110halp@gmail.com>
2022-02-14 18:24:05 +0000

    Merge Dialog classes into FeedBinGUI.java

--- src/gui/FeedBinGUI.java
+++ src/gui/FeedBinGUI.java
@@ -3,6 +3,7 @@ package gui;
 import bin.FeedBin;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -99,3 +100,170 @@ public class FeedBinGUI extends JFrame {
     }
 
 } // End Class FeedBinGUI
+
+/*
+Inspect Menu Option Dialog Class
+ */
+class InspectDialog extends JDialog {
+
+    private FeedBin bin;
+
+    private Box boxInfo;
+    private Box boxButton;
+    private JPanel panel;
+    private JLabel[] label;
+    private JButton OKButton;
+
+    /*
+    InspectDialog Constructor
+     */
+    InspectDialog(FeedBinGUI parent, boolean modal, FeedBin binObject) {
+
+        super(parent, "Bin Inspection", modal); // Call Superclass Constructor
+
+        this.bin = binObject;
+
+        getContentPane().setLayout(new BoxLayout(getContentPane(), BoxLayout.Y_AXIS));
+
+        this.panel = new JPanel();
+        this.panel.setLayout(new GridLayout(4, 2));
+
+        this.label = new JLabel[8];
+        this.label[0] = new JLabel("Bin Number");
+        this.label[2] = new JLabel("Contains");
+        this.label[4] = new JLabel("Maximum Volume");
+        this.label[6] = new JLabel("Current Volume");
+        this.label[1] = new JLabel(String.valueOf(bin.getBinNumber()));
+        this.label[3] = new JLabel(bin.getProductName());
+        this.label[5] = new JLabel(String.valueOf(bin.getMaxVolume()));
+        this.label[7] = new JLabel(String.valueOf(bin.getCurrentVolume()));
+
+        this.OKButton = new JButton("OK");
+        OKButton.addActionListener(e -> dispose());
+
+        for (JLabel idLabel : label) this.panel.add(idLabel); // Labels go into a 4x2 Grid
+
+        this.boxInfo = new Box(BoxLayout.Y_AXIS);
+        this.boxButton = new Box(BoxLayout.Y_AXIS);
+        this.boxInfo.add(panel);
+        this.boxButton.add(OKButton);
+        getContentPane().add(boxInfo);
+        getContentPane().add(boxButton);
+
+        pack();
+
+    } // End InspectDialog Constructor
+
+} // End InspectDialog Class
+
+class FillDialog extends JDialog implements ActionListener {
+
+    private FeedBin bin;
+    private JTextField input;
+    private JPanel panel1;
+    private JPanel panel2;
+    private JLabel label;
+    private JButton applyButton;
+    private JButton clearButton;
+    private JButton cancelButton;
+
+    /*
+    FillDialog Constructor
+     */
+    FillDialog(FeedBinGUI parent, boolean modal, FeedBin binObject) {
+
+        super(parent, "Bin Filling", modal); // Call Superclass Constructor
+
+        this.bin = binObject;
+
+        this.panel1 = new JPanel();
+        this.panel2 = new JPanel();
+
+        this.label = new JLabel("How much is to be added?");
+        this.input = new JTextField(10);
+
+        this.applyButton = new JButton("Apply");
+        this.clearButton = new JButton("Clear");
+        this.cancelButton = new JButton("Cancel");
+
+        this.applyButton.addActionListener(this);
+        this.clearButton.addActionListener(this);
+        this.cancelButton.addActionListener(this);
+
+        this.panel1.add(label);
+        this.panel2.add(input);
+
+        getContentPane().add(panel1, "North");
+
+        this.panel2.add(applyButton);
+        this.panel2.add(clearButton);
+        this.panel2.add(cancelButton);
+
+        getContentPane().add(panel2, "South");
+
+        pack();
+
+    }
+
+    /*
+    Button Event Handler
+     */
+    @Override
+    public void actionPerformed(ActionEvent event) {
+
+        double volume;
+        String outcome;
+        String butLabel = event.getActionCommand();
+
+        if (butLabel.equals("Apply")) {
+
+            try {
+
+                volume = Double.parseDouble(input.getText());
+
+                if (volume < 0.0d) throw new NumberFormatException();
+
+                if (! bin.addProduct(volume)) throw new FillException(); // Interact with the Bin Object here
+
+                JOptionPane.showMessageDialog(
+                        this,
+                        "Addition Confirmed",
+                        "Fill Report",
+                        JOptionPane.INFORMATION_MESSAGE,
+                        null);
+
+                dispose();
+
+            } catch (NumberFormatException e) { // May be thrown by parseDouble()
+
+                JOptionPane.showMessageDialog(
+                        this,
+                        "Not a valid number",
+                        "Fill Report",
+                        JOptionPane.WARNING_MESSAGE,
+                        null
+                );
+
+            } catch (FillException e) {
+
+                JOptionPane.showMessageDialog(
+                        this,
+                        "Not enough room in the bin",
+                        "Fill Report",
+                        JOptionPane.WARNING_MESSAGE,
+                        null
+                );
+
+            } // End of Apply Button Handling
+
+        } else if (butLabel.equals("Clear")) {
+
+            this.input.setText("");
+
+        } else dispose(); // Must have pressed the cancel button
+
+    }
+
+} // Class FillDialog
+
+class FillException extends RuntimeException {}
