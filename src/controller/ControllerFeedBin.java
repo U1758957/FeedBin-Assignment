12602099c0a63e8fc829c0719b6996f30bc3bad5
UggUggDev <10110halp@gmail.com>
2022-02-17 22:30:53 +0000

    Add methods between bin controller and feed bin class

--- src/controller/ControllerFeedBin.java
+++ src/controller/ControllerFeedBin.java
@@ -10,6 +10,45 @@ public class ControllerFeedBin implements Runnable {
         this.bins = bins;
     }
 
+    private void setProductName(int binNumber, String newProductName) {
+
+        boolean isNameChanged = this.bins[binNumber].setProductName(newProductName);
+
+        if (! isNameChanged); // TODO: 17/02/2022 Notify the client that they could not change the name
+
+    }
+
+    private void flush(int binNumber) {
+        this.bins[binNumber].flush();
+    }
+
+    private void addProduct(int binNumber, double volume) {
+
+        boolean isProductAdded = this.bins[binNumber].addProduct(volume);
+
+        if (! isProductAdded); // TODO: 17/02/2022 Notify the client that they could not add the product
+
+    }
+
+    private void removeProduct(int binNumber, double volume) {
+
+        double removedProduct = this.bins[binNumber].removeProduct(volume);
+
+        // TODO: 17/02/2022 Place the removed volume into the buffer when it's made
+
+    }
+
+    private void inspectBin(int binNumber) {
+
+        int binNo = this.bins[binNumber].getBinNumber(); // Kind of moot, but doing it for sake of completeness
+        String productName = this.bins[binNumber].getProductName();
+        double maxVolume = this.bins[binNumber].getMaxVolume();
+        double currentVolume = this.bins[binNumber].getCurrentVolume();
+
+        // TODO: 17/02/2022 Create dialogue box showing the stats for the bin for the client
+
+    }
+
     @Override
     public void run() {
 
