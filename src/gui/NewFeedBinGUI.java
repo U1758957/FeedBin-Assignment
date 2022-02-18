a1913d1efb98da5435eaeb3dbb3f5e226d3c8c5b
UggUggDev <10110halp@gmail.com>
2022-02-18 15:58:08 +0000

    Add product parsing implemented

--- src/gui/NewFeedBinGUI.java
+++ src/gui/NewFeedBinGUI.java
@@ -6,6 +6,8 @@ import supervisor.ControllerSupervisor;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.concurrent.CountDownLatch;
@@ -75,6 +77,24 @@ public class NewFeedBinGUI extends JFrame {
 
             this.panelBinController.setVisible(false);
             this.panelSupervisor.setVisible(true);
+
+        });
+
+        buttonAddProduct.addActionListener(e -> {
+
+            try {
+
+                double volume = Double.parseDouble(textFieldAddProduct.getText());
+
+            } catch (NumberFormatException ex) {
+
+                JOptionPane.showMessageDialog(
+                        this,
+                        "Adding product input must be a number!",
+                        "Warning",
+                        JOptionPane.WARNING_MESSAGE);
+
+            }
 
         });
 
