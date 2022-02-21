944e2f95a40c833d6ea44723ba436a361170deb8
UggUggDev <10110halp@gmail.com>
2022-02-21 15:16:38 +0000

    ControllerSupervisor template initialised

--- src/supervisor/ControllerSupervisor.java
+++ src/supervisor/ControllerSupervisor.java
@@ -7,8 +7,27 @@ public class ControllerSupervisor implements Runnable {
 
     private final ModelFeedBin[] bins;
 
+    private volatile String batch;
+    private volatile int operation;
+
     public ControllerSupervisor(ModelFeedBin[]bins) {
         this.bins = bins;
+        this.batch = "";
+        this.operation = -1;
+    }
+
+    /*
+
+    Operation IDs and their roles for the ControllerSupervisor:
+
+    0 : Adding a batch
+    1 : Processing the next batch
+    2 : Inspecting all the bins
+
+     */
+    public void issueOrder(String batch, int operation) {
+        this.batch = batch;
+        this.operation = operation;
     }
 
     @Override
@@ -16,7 +35,25 @@ public class ControllerSupervisor implements Runnable {
 
         while (NewFeedBinGUI.controllerLatch.getCount() > 0) {
 
-            // Critical section here
+            if (operation > -1) {
+
+                switch (operation) {
+
+                    case 0:
+                        break;
+
+                    case 1:
+                        break;
+
+                    case 2:
+                        break;
+
+                }
+
+                this.operation = -1;
+                NewFeedBinGUI.guiLatch.countDown();
+
+            }
 
             try {
                 Thread.sleep(1L);
